30085af31d70cfbe0a0a4dd2ff57840b84683c1e
Texnomic <me@texnomic.com>
2020-06-13 23:37:52 +0200

    ✔ Fixed Bug In ProxyResponsibilityChainOptions.

--- Texnomic.SecureDNS.Servers/Proxy/Options/ProxyResponsibilityChainOptions.cs
+++ Texnomic.SecureDNS.Servers/Proxy/Options/ProxyResponsibilityChainOptions.cs
@@ -10,8 +10,15 @@ namespace Texnomic.SecureDNS.Servers.Proxy.Options
     {
         public List<string> Middlewares { get; set; }
 
-        public List<Type> GetMiddlewares() => AppDomain.CurrentDomain.GetAssemblies()
-                                                                     .Single(Assembly => Assembly.FullName.StartsWith("Texnomic.SecureDNS.Middlewares")).GetTypes()
-                                                                     .Where(Type => Type.GetInterfaces().Contains(typeof(IAsyncMiddleware<IMessage, IMessage>))).ToList();
+        public List<Type> GetMiddlewares()
+        {
+            var Types = AppDomain.CurrentDomain.GetAssemblies()
+                                                                .Single(Assembly => Assembly.FullName.StartsWith("Texnomic.SecureDNS.Middlewares"))
+                                                                .GetTypes()
+                                                                .Where(Type => Type.GetInterfaces().Contains(typeof(IAsyncMiddleware<IMessage, IMessage>)))
+                                                                .ToLookup(Type => Type.Name, Type => Type);
+
+            return Middlewares.SelectMany(Middleware => Types[Middleware]).ToList();
+        }
     }
 }
